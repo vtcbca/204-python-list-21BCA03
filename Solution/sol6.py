94382c096c3d169875c278c42ac5136de2ccc5a6
21BCA03 <96902340+21BCA03@users.noreply.github.com>
2022-03-31 10:14:07 +0530

    solution6 uploaded

---
+++ Solution/sol6.py
@@ -0,0 +1,18 @@
+# write a script to enter any number and appropriate day name
+a=int(input("Enter any Number:"))
+if(a==1):
+      print("Monday")
+elif(a==2):
+      print("Tuesday")
+elif(a==3):
+      print("Wendnesday")
+elif(a==4):
+      print("Thursady")
+elif(a==5):
+      print("Friday")
+elif(a==6):
+      print("Saturday")
+elif(a==7):
+      print("Sunday")
+else:
+    print("There is no Days")
